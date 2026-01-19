326579ea62ff538d5b44f4f67f3697fa72d789bb
guiarthur <arthur.guillaume@ynov.com>
2026-01-19 12:02:34 +0100

    msg

--- join-customers-invoices.sql
+++ join-customers-invoices.sql
@@ -1,2 +1,3 @@
 SELECT C.FirstName, C.LastName, C.Company, I.Total AS InvoicesTotalPrices FROM customers AS C
-JOIN invoices AS I ON C.CustomerId = I.CustomerId;
+JOIN invoices AS I ON C.CustomerId = I.CustomerId
+WHERE C.Company IS NOT NULL;
