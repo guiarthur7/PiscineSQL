3fb817c828e365643d5155a297cf4857640521d7
guiarthur <arthur.guillaume@ynov.com>
2026-01-19 11:10:24 +0100

    msg

--- select-customers-where-and.sql
+++ select-customers-where-and.sql
@@ -1,3 +1,4 @@
-SELECT FirstName, LastName FROM customers AS C
+SELECT C.FirstName, C.LastName FROM customers AS C
 JOIN employees AS E ON C.SupportRepId = E.EmployeeId
 WHERE E.FirstName = 'Jane' AND E.LastName = 'Peacock';
+
