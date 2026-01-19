a84788b4942b87ceca0d731280cf12c1cb306539
guiarthur <arthur.guillaume@ynov.com>
2026-01-19 10:13:53 +0100

    msg

--- update-employee.sql
+++ update-employee.sql
@@ -1,3 +1,4 @@
 UPDATE employees
 SET Title = 'IT Manager'
+SET ReportsTo = SELECT id FROM employees WHERE FirstName = 'Adams' AND LastName = 'Andrew'
 WHERE FirstName = 'Robert' AND LastName = 'King'
