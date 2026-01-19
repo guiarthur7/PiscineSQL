1c64de467e328f9a083ca15255a855903b52ac93
guiarthur <arthur.guillaume@ynov.com>
2026-01-19 12:25:45 +0100

    msg

--- double-join.sql
+++ double-join.sql
@@ -1,5 +1,5 @@
 SELECT DISTINCT T.Name AS TrackName, P.Name AS PlaylistName FROM playlists AS P
 JOIN playlist_track AS PT ON P.PlaylistId = PT.PlaylistId
 JOIN tracks AS T ON T.TrackId = PT.TrackId
-WHERE P.Name = 'TV Show'
+WHERE P.Name = 'TV Shows'
 LIMIT 100;
