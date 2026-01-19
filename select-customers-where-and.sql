17672384c530bbee4d7646283098430853528445
guiarthur <arthur.guillaume@ynov.com>
2026-01-19 11:15:32 +0100

    msg

--- select-customers-where-and.sql
+++ select-customers-where-and.sql
@@ -1,4 +1,4 @@
 SELECT C.FirstName, C.LastName FROM customers AS C
 JOIN employees AS E ON C.SupportRepId = E.EmployeeId
-WHERE C.Country = 'USA' E.FirstName = 'Jane' AND E.LastName = 'Peacock';
+WHERE C.Country = 'USA' AND E.FirstName = 'Jane' AND E.LastName = 'Peacock';
 
