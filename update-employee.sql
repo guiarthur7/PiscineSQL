dc5dcdf2024162b682e33f356eab5d4edb350274
guiarthur <arthur.guillaume@ynov.com>
2026-01-19 10:15:04 +0100

    msg

--- update-employee.sql
+++ update-employee.sql
@@ -1,4 +1,4 @@
 UPDATE employees
-SET Title = 'IT Manager'
-SET ReportsTo = SELECT id FROM employees WHERE FirstName = 'Adams' AND LastName = 'Andrew'
-WHERE FirstName = 'Robert' AND LastName = 'King'
+SET Title = 'IT Manager',
+    ReportsTo = (SELECT EmployeeId FROM employees WHERE FirstName = 'Andrew' AND LastName = 'Adams')
+WHERE FirstName = 'Robert' AND LastName = 'King';
