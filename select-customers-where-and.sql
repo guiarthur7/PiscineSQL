18d4605b555cece0d9652efaeb3bbd12825d4973
guiarthur <arthur.guillaume@ynov.com>
2026-01-19 11:14:20 +0100

    msg

--- select-customers-where-and.sql
+++ select-customers-where-and.sql
@@ -1,4 +1,4 @@
 SELECT C.FirstName, C.LastName FROM customers AS C
 INNER JOIN employees AS E ON C.SupportRepId = E.EmployeeId
-WHERE E.FirstName = 'Jane' AND E.LastName = 'Peacock';
+WHERE C.Country = 'USA' E.FirstName = 'Jane' AND E.LastName = 'Peacock';
 
