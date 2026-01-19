0a53ea51e67a9f867092cc3d09d73e7b703c247d
guiarthur <arthur.guillaume@ynov.com>
2026-01-19 11:34:41 +0100

    msg

--- select-tracks-order-by.sql
+++ select-tracks-order-by.sql
@@ -1,4 +1,4 @@
 SELECT T.Name, T.Milliseconds FROM tracks AS T
-JOIN artists AS A ON Al.ArtistId = A.ArtistId
+JOIN artists AS A ON T.ArtistId = A.ArtistId
 WHERE A.Name = 'AC/DC'
 ORDER BY T.Milliseconds ASC;
