9ee096e316ccb959192a3cfed18405072ea24c9c
guiarthur <arthur.guillaume@ynov.com>
2026-01-19 12:14:30 +0100

    msg

--- join-albums-tracks.sql
+++ join-albums-tracks.sql
@@ -1,4 +1,4 @@
-SELECT Al.Title AS AlbumName, T.Name AS TrackName, T.Milliseconds FROM albums AS A
+SELECT A.Title AS AlbumName, T.Name AS TrackName, T.Milliseconds FROM albums AS A
 INNER JOIN tracks AS T ON A.AlbumId = T.AlbumId
 ORDER BY T.Milliseconds ASC
 LIMIT 50;
